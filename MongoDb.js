0d75018a32e615c587bafa32d6675282be53e8cb
Vinayak1205 <143931917+Vinayak1205@users.noreply.github.com>
2025-04-24 15:52:49 +0530

    Add files via upload

---
+++ MongoDb.js
@@ -0,0 +1,22 @@
+const mongodb=require('mongodb')
+
+// const connectionUrl = "mongodb+srv://devanshkhetan9:eGujuNNcNbF2UZda@cluster0.iamg6.mongodb.net/NoteGo?retryWrites=true&w=majority";
+const connectionUrl = "mongodb://localhost:27017/NoteGo";
+
+const client=new mongodb.MongoClient(connectionUrl)
+
+var db;
+
+try{
+    
+    client.connect();
+    console.log("Connected to Mongodb")
+    db=client.db()
+}
+
+catch(err)
+{
+    console.log(err)
+}
+
+module.exports=db
